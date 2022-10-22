19bc87caa4ea86eb2f5fd32c5ee909dad481816e
Dmitry Gritsenko <k01419q45@ya.ru>
2022-10-22 08:08:55 +0400

    It's 8am and I'm too lazy to split commits. Read 1.3.0 change log

--- rgx/__init__.py
+++ rgx/__init__.py
@@ -1,1 +1,2 @@
-from .entities import pattern, NamedPattern as named, Meta as meta, group_reference as reference, ConditionalPattern as conditional, char_range
+from .entities import pattern, NamedPattern as named, Meta as meta, group_reference as reference, ConditionalPattern as conditional, char_range, UnicodeMeta as unicode_meta
+umeta = unicode_meta
